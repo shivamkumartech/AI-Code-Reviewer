21bf41f91b23e3c6d7ea21905b2446f3d3e6487a
Shivam Kumar <shivanshkumarchauhan@gmail.com>
2026-08-24 02:00:07 +0530

    feat: initialize frontend and backend project structure with core boilerplate files

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,60 @@
+import prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+import { useState, useEffect } from "react";
+import EditorComponent from "react-simple-code-editor";
+import axios from "axios";
+
+const Editor = EditorComponent.default || EditorComponent;
+
+function App() {
+  const [code, setCode] = useState(` function sum() {
+  return 1 + 1
+}`);
+
+  useEffect(() => {
+    prism.highlightAll();
+  }, []);
+
+  const reviewCode = async () => {
+    
+    const response = await axios.post("http://localhost:3000/ai/get-review", { code })
+
+    console.log(response.data)
+  }
+
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={(code) => setCode(code)}
+              highlight={(code) =>
+                prism.highlight(code, prism.languages.javascript, "javascript")
+              }
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%",
+              }}
+            />
+          </div>
+          <div
+            onClick={reviewCode}
+            className="review">
+            Review
+          </div>
+        </div>
+        <div className="right"></div>
+      </main>
+    </>
+  );
+}
+
+export default App;
+
