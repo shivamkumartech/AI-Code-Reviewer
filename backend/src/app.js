beb9bd91407850b59cdd884c59300e680d3b0ba6
Shivam Kumar <shivanshkumarchauhan@gmail.com>
2026-08-24 01:16:37 +0530

    feat: implement backend AI code review service using Google Gemini API

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,8 @@ import aiRoutes from "./routes/ai.routes.js";
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/ai", aiRoutes);
 
 export default app;
--- backend/src/controllers/ai.controller.js
+++ backend/src/controllers/ai.controller.js
@@ -1,14 +1,20 @@
 import aiService from "../services/ai.service.js"
 
 export const getReview = async (req, res) => {
-    
-    const prompt = req.query.prompt
+    try {
+        const {code}  = req.body;
 
-    if (!prompt) {
-        return res.status(400).send("Prompt is Required")
+        if (!code) {
+            return res.status(400).send("Code is Required");
+        }
+
+        const response = await aiService(code);
+
+        res.send(response);
+
+    } catch (error) {
+        console.error("AI Service Error: ", error.message);
+        
+        res.status(500).send("Something went wrong while generating the review.");
     }
-
-    const responce = await aiService(prompt)
-
-    res.send(responce)
 }
